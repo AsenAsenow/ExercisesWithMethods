b72c79c4c39a57688c150f9e4586740ec0c23e12
Asen Asenow <asen.asenow.87@gmail.com>
2018-01-19 12:35:24 +0200

    Update class NthDigitFind

--- ExercisesMethods/src/NthDigitFind.java
+++ ExercisesMethods/src/NthDigitFind.java
@@ -2,23 +2,22 @@ import java.util.Scanner;
 
 public class NthDigitFind {
     public static void main(String[] args) {
-        Scanner scan  = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         int number = Integer.parseInt(scan.nextLine());
         int index = Integer.parseInt(scan.nextLine());
 
-        System.out.println(findNthDigit(number,index));
+        System.out.println(findNthDigit(number, index));
     }
 
-    static int findNthDigit (int number, int index) {
+    static int findNthDigit(int number, int index) {
 
-        int result = 0;
 
         int dividerResult = 0;
-        while (index!=1){
-           number=number/10;
+        while (index != 1) {
+            number = number / 10;
             index--;
         }
-        dividerResult=number%10;
+        dividerResult = number % 10;
 
         return dividerResult;
     }
